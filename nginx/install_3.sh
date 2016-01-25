02fdf766ab94a8e2ec3870e4dabe9535b1ddd9a8
William <gcphost@users.noreply.github.com>
2016-01-24 21:01:16 -0800

    Update install_3.sh

--- nginx/install_3.sh
+++ nginx/install_3.sh
@@ -1,4 +1,69 @@
-echo ‘cgi.fix_pathinfo=0’ >> /etc/php/7.0/fpm/php.ini
+mkdir -p /var/www/html
+
+mv /etc/nginx/conf.d/default.conf /etc/nginx/default.conf-bkup
+cat <<EOF > /etc/nginx/conf.d/default.conf
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+
+    root /var/www/html/public;
+    index index.php index.html index.htm;
+
+    server_name localhost;
+    charset   utf-8;
+
+    gzip on;
+    gzip_vary on;
+    gzip_disable "msie6";
+    gzip_comp_level 6;
+    gzip_min_length 1100;
+    gzip_buffers 16 8k;
+    gzip_proxied any;
+    gzip_types
+        text/plain
+        text/css
+        text/js
+        text/xml
+        text/javascript
+        application/javascript
+        application/x-javascript
+        application/json
+        application/xml
+        application/xml+rss;
+
+    location / {
+
+        location ~* \.(?:jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc|svg|woff|woff2|ttf)\$ {
+          expires 1M;
+          access_log off;
+          add_header Cache-Control "public";
+        }
+
+        location ~* \.(?:css|js)\$ {
+          expires 7d;
+          access_log off;
+          add_header Cache-Control "public";
+        }
+
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php\$ {
+        try_files \$uri /index.php =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)\$;
+        fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny  all;
+    }
+
+}
+EOF
+echo 'cgi.fix_pathinfo=0' >> /etc/php/7.0/fpm/php.ini
 sed -i 's/www-data/nginx/g' /etc/php/7.0/fpm/pool.d/www.conf
 
 /etc/init.d/nginx restart
