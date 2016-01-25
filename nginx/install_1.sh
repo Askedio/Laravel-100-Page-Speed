b995354384904fb0be2e85ddad20383d11bb9ccd
William <gcphost@users.noreply.github.com>
2016-01-24 21:00:27 -0800

    Update install_1.sh

--- nginx/install_1.sh
+++ nginx/install_1.sh
@@ -1,8 +1,1 @@
 add-apt-repository ppa:ondrej/php
-echo 'deb http://nginx.org/packages/mainline/ubuntu/ trusty nginx' >> /etc/apt/sources.list
-echo 'deb-src http://nginx.org/packages/mainline/ubuntu/ trusty nginx' >> /etc/apt/sources.list
-wget -q -O- http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-
-apt-get update
-apt-get -y install nginx php7.0 php7.0-mysql php7.0-fpm
-mkdir -p /var/www/html
