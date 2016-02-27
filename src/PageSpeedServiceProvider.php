0413ddebcec2c4a426a6438b4e8c6ba3501e97fe
William <gcphost@gmail.com>
2016-02-27 13:31:58 -0500

    Applied fixes from StyleCI

--- src/PageSpeedServiceProvider.php
+++ src/PageSpeedServiceProvider.php
@@ -7,31 +7,28 @@ use Illuminate\Support\ServiceProvider;
 class PageSpeedServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
+       * Bootstrap any application services.
+       *
+       * @return void
+       */
       public function boot(\Illuminate\Routing\Router $router, \Illuminate\Contracts\Http\Kernel $kernel)
       {
           $kernel->prependMiddleware(\Askedio\Laravel100PageSpeed\Http\Middleware\Minify::class);
           $kernel->pushMiddleware(\Askedio\Laravel100PageSpeed\Http\Middleware\Minify::class);
           $router->middleware('minify', \Askedio\Laravel100PageSpeed\Http\Middleware\Minify::class);
 
-          if (! $this->app->routesAreCached())
-          {
+          if (!$this->app->routesAreCached()) {
               require __DIR__.'/routes.php';
           }
 
-        $this->publishes([
+          $this->publishes([
             __DIR__.'/config/minify.php' => config_path('minify.php'),
         ]);
 
-        $this->mergeConfigFrom(
-            __DIR__.'/config/minify.php' , 'minify'
+          $this->mergeConfigFrom(
+            __DIR__.'/config/minify.php', 'minify'
         );
-
       }
-
 
     /**
      * Register any application services.
